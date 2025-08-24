8725ce47345dc44fdfd0438f771b444b20ad81df
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-08-25 07:50:33 +0800

    Done creating and testing repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+// Repeats the string a given number of times
+const repeatString = function(strText, intTimes) {
 
+    if(intTimes < 0) {
+        return "ERROR";
+    }
+
+    let strRepeat = "";
+
+    for(let i = 0; i < intTimes; i++) {
+        strRepeat += strText;
+    }
+
+    return strRepeat;
 };
+
+const STR_REPEAT_HEY = repeatString('hey', 3); // returns 'heyheyhey'
+console.log(STR_REPEAT_HEY);
 
 // Do not edit below this line
 module.exports = repeatString;
