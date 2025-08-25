40b3370a70abd4e3ae390225a9ccdc9224f75fba
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-08-25 10:30:06 +0800

    Done creating and testing removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrGiven, ...removeElement) {
+    return arrGiven.filter(element => removeElement.indexOf(element) < 0);
+    // or return arrGiven.filter(element => !removeElement.includes(element));
 };
+
+const arrGiven = removeFromArray([1, 2, 3, 4], 3);
+console.log(arrGiven);
+
+const arrGiven2 = removeFromArray([1, 2, 3, 4], 3, 2);
+console.log(arrGiven2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
