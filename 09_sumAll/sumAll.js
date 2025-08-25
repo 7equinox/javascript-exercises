3ff75448fa0827f6b0873e5869c1e0301d4c3aad
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-08-25 11:02:50 +0800

    Done creating and testing sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+// function of the sum of every integer between them, inclusively
+const sumAll = function(intFirstNum, intSecondNum) {
 
+    // two numbers must be positive
+    const IS_POS_NUM = intFirstNum > 0 &&
+                        intSecondNum > 0;
+
+    // two numbers must be integer
+    const IS_INT_NUM = Number.isInteger(intFirstNum) &&
+                        Number.isInteger(intSecondNum);
+
+    if(!IS_POS_NUM ||
+        !IS_INT_NUM) {
+        return "ERROR";
+    }
+
+    // count the number of terms
+    const INT_NUM_OF_TERMS = Math.abs(intSecondNum - intFirstNum) + 1;
+
+    // sum of an arithmetic seq
+    return INT_NUM_OF_TERMS / 2 * (intFirstNum + intSecondNum);
 };
 
 // Do not edit below this line
