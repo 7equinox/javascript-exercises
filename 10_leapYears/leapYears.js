644a47eed94085045f18b1e86095e831aa2739ec
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-08-25 11:16:30 +0800

    Done creating and leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+// function to check if a year is a leap year
+const leapYears = function(intYear) {
+    const IS_DIVISIBLE_BY_FOUR = (intYear % 4 === 0);
+    const IS_DIVISIBLE_BY_HUNDRED = (intYear % 100 === 0);
+    const IS_DIVISIBLE_BY_400 = (intYear % 400 === 0);
 
+    return IS_DIVISIBLE_BY_FOUR && (!IS_DIVISIBLE_BY_HUNDRED || IS_DIVISIBLE_BY_400);
 };
 
 // Do not edit below this line
