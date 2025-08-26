bd60f1a86bf7e493836dea8b315a408e64435671
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-08-26 08:54:55 +0800

    Done creating and testing tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const roundToOneDecimalPlace = function(fltTemp) {
+  const FLT_ROUNDED_TEMP = Math.round(fltTemp * 10) / 10;
+  return FLT_ROUNDED_TEMP;
+}
+
+const convertToCelsius = function(intFahrenheit) {
+  const FLT_FAHRENHEIT = (intFahrenheit - 32) * 5 / 9;
+  const FLT_ROUNDED_FAHRENHEIT = roundToOneDecimalPlace(FLT_FAHRENHEIT);
+  return FLT_ROUNDED_FAHRENHEIT;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(intCelsius) {
+  const FLT_CELSIUS = (intCelsius * 9 / 5) + 32;
+  const FLT_ROUNDED_CELSIUS = roundToOneDecimalPlace(FLT_CELSIUS);
+  return FLT_ROUNDED_CELSIUS;
 };
 
 // Do not edit below this line
