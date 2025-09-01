b8703621fba3d0731fcdadd5aa7aa5a7fac5f8f5
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-09-01 22:55:04 +0800

    Add and test fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+// Function to get a specific member of the Fibonacci seq
+const fibonacci = function(num) {
+    
+    // Convert str to num if given parameter is string
+    if(typeof num !== "number") num = Number(num);
+    
+    // Best case: if 0 or negative numbers
+    if(num < 0) return "OOPS";
+    if(num === 0) return 0;
 
+    let intPrevNum = 1;
+    let intCurrNum = 1;
+
+    for(let i = 2; i < num; i++)
+    {
+        const intNewFibo = intPrevNum + intCurrNum;
+        intPrevNum = intCurrNum;
+        intCurrNum = intNewFibo;
+    }
+
+    return intCurrNum;
 };
+
+console.log( fibonacci("0") );
 
 // Do not edit below this line
 module.exports = fibonacci;
